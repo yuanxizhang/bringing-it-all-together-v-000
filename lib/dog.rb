19cc8ca19359531a59390077de95719749cdeacf
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-23 08:22:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
   def self.find_by_name(name) 
     sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Dog.new(id: result[0], name: result[1], nreed: result[2])
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end
   
   def update
